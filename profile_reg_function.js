f4378d4da71dd828909731bfa1a32b81fbb76253
NaufalRmdhn21 <141596896+NaufalRmdhn21@users.noreply.github.com>
2023-10-02 13:19:19 +0700

    Add files via upload

---
+++ profile_reg_function.js
@@ -0,0 +1,7 @@
+
+function profile(namaDepan, namaBelakang, email) {
+    console.log("Nama : " + namaDepan + " "+ namaBelakang)
+    console.log("Email : "+ email)
+}
+
+profile("Naufal","Ramadhan","openg@gmail.com")
